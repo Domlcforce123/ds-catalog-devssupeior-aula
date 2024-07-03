10f7065e6be2fd4bc4461aa707066ec93bcf4ec7
Domlcforce123 <deoliveiracoelholucas210@gmail.com>
2024-07-03 10:57:48 -0300

    h2 database

---
+++ backend/src/main/java/com/backend/resources/CategoryResources.java
@@ -0,0 +1,28 @@
+package com.backend.resources;
+
+
+import com.backend.Category;
+import com.backend.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController//spring web
+@RequestMapping(value = "/categories")
+public class CategoryResources {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+}
